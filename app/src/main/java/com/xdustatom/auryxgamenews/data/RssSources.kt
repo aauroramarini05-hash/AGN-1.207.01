c188b4d1bc9191e06d4b1bb35f125e2f68ade458
aauroramarini05-hash <aaurora.marini05@gmail.com>
2026-02-07 20:34:31 +0100

    Update RssSources.kt

--- app/src/main/java/com/xdustatom/auryxgamenews/data/RssSources.kt
+++ app/src/main/java/com/xdustatom/auryxgamenews/data/RssSources.kt
@@ -1,12 +1,29 @@
 package com.xdustatom.auryxgamenews.data
 
-data class RssSource(val name: String, val url: String)
+import org.simpleframework.xml.Element
+import org.simpleframework.xml.ElementList
+import org.simpleframework.xml.Root
 
-object RssSources {
-    val sources = listOf(
-        RssSource("IGN", "https://feeds.ign.com/ign/all"),
-        RssSource("Eurogamer", "https://www.eurogamer.net/feed"),
-        RssSource("Multiplayer.it", "https://www.multiplayer.it/rss/news.xml"),
-        RssSource("Rockstar Newswire", "https://www.rockstargames.com/newswire.rss")
-    )
-}
+@Root(name = "rss", strict = false)
+data class RssFeed(
+    @field:Element(name = "channel")
+    var channel: Channel? = null
+)
+
+@Root(name = "channel", strict = false)
+data class Channel(
+    @field:ElementList(entry = "item", inline = true, required = false)
+    var items: List<RssItem>? = null
+)
+
+@Root(name = "item", strict = false)
+data class RssItem(
+    @field:Element(name = "title", required = false)
+    var title: String? = null,
+
+    @field:Element(name = "link", required = false)
+    var link: String? = null,
+
+    @field:Element(name = "description", required = false)
+    var description: String? = null
+)
