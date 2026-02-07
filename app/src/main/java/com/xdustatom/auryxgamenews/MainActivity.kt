816a75d345895f788a765007a9c7bd284d507c9f
aauroramarini05-hash <aaurora.marini05@gmail.com>
2026-02-07 20:33:52 +0100

    Update MainActivity.kt

--- app/src/main/java/com/xdustatom/auryxgamenews/MainActivity.kt
+++ app/src/main/java/com/xdustatom/auryxgamenews/MainActivity.kt
@@ -3,24 +3,13 @@ package com.xdustatom.auryxgamenews
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.runtime.Composable
 import com.xdustatom.auryxgamenews.ui.NewsScreen
-import com.xdustatom.auryxgamenews.ui.theme.AuryxGameNewsTheme
 
 class MainActivity : ComponentActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContent {
-            AuryxGameNewsTheme {
-                App()
-            }
+            NewsScreen()
         }
     }
-}
-
-@Composable
-fun App() {
-    NewsScreen()
 }
